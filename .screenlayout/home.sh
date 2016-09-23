57db03035d4bf403ba8eb7cca9630b8e5b1c4c8f
Ivan Stefanischin <ivan.stefanischin@bevuta.com>
2016-09-23 14:18:45 +0200

    add screenlayouts

---
+++ .screenlayout/home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI1 --off --output LVDS1 --mode 1440x900 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --mode 1680x1050 --pos 1440x0 --rotate normal
---
+++ .screenlayout/office.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI1 --off --output LVDS1 --mode 1440x900 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP1 --mode 2560x1440 --pos 1440x0 --rotate normal --output VGA1 --off
