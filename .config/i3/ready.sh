8b0726cf11b585c47dc6dbf04d842ce3e755d224
Ivan Stefanischin <ivan@stefanischin.eu>
2017-08-18 18:16:15 +0200

    commit removement of window-manager systemd target and the sleep hack

--- .config/i3/ready.sh
+++ .config/i3/ready.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-# HACK: Wait a little bit, it seems the window manager didn't startup
-# completely until now
-sleep 5
-
-# Make modification environment variables visible for systemd user services
-systemctl --user import-environment
-
-# Activate the window-manager systemd-target to run services which
-# dependent on a window manager
-systemctl --user start window-manager.target
-
 # Start the tmux client
 i3-sensible-terminal -class tmux -e tmux_session &
 
