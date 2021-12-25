dcf928b5d66b6af982ec0293006f63ac0d954f55
Ivan Stefanischin <ivan@stefanischin.eu>
2021-12-25 14:15:38 +0100

    use tmpfs as storage for cache files during runtime
    Synchronize the cache frequently and on shutdown.

---
+++ bin/volatize.sh
@@ -0,0 +1,51 @@
+#!/bin/env sh
+
+static=.static-$2
+link=$2
+volatile=/dev/shm/$USER-$2
+
+IFS=
+set -efu
+
+save () {
+    rsync -av --delete ./$1/ ./$2/
+}
+
+start () {
+    if [ ! -r $volatile ]; then
+        mkdir -m0700 $volatile
+    fi
+
+    if [ "$(readlink $link)" != "$volatile" ]; then
+        mv $link $static
+        ln -s $volatile $link
+        save ./$static/ ./$link/
+    else
+        save $link $static
+    fi
+    echo "$(date) '$link' synchronized"
+}
+
+stop () {
+    if [ "$(readlink $link)" = "$volatile" ]; then
+        save $link $static
+        rm $link
+        mv $static $link
+    fi
+}
+
+case $1 in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    *)
+        echo "Usage: $0 [FOLDER] [ACTION]"
+        echo ""
+        echo "ACTIONS"
+        echo "    start -"
+        echo "    stop -"
+        exit 2
+esac
