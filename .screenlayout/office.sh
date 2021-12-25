d5e47ae218b37cb05c98a72d93448c8bd17fd1af
Ivan Stefanischin <ivan@stefanischin.eu>
2021-12-25 19:26:58 +0100

    adjust screenlayout profile for office

--- .screenlayout/office.sh
+++ .screenlayout/office.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-xrandr --output DP2-2 --mode 1920x1080 --pos 0x360 --rotate normal
 xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x360 --rotate normal
 xrandr --output DP2-1 --mode 2560x1440 --pos 3840x0 --rotate normal
+xrandr --output DP2-2 --mode 1920x1080 --pos 0x360 --rotate normal
+xrandr --output DP2-2 --pos 0x360
+xrandr --output DP2-1 --pos 3840x0
+xrandr --output eDP1 --pos 1920x360
 xinput --map-to-output 'Weida Hi-Tech                CoolTouchR System           ' DP2-2
