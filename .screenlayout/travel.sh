7146a29bcc719e548a00311670a21abbf467f7c4
Ivan Stefanischin <ivan@stefanischin.eu>
2022-12-23 21:40:53 +0100

    update screenlayout

--- .screenlayout/travel.sh
+++ .screenlayout/travel.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
-xinput --map-to-output 'Weida Hi-Tech                CoolTouchR System           ' HDMI2
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x360 --rotate normal --output DP1 --off --output DP2 --off --output DP2-1 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP2-2 --off --output DP2-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
